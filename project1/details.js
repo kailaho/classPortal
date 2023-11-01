17b4767d72027d4309a32f4d5537953f80e0c2fb
kailaho <kaho3670@colorado.edu>
2023-11-01 11:35:55 -0600

    update 3

--- project1/details.js
+++ project1/details.js
@@ -1,5 +1,5 @@
 // Load 3D Scene
-import * as THREE from './node_modules/three/build/three.module.js';
+import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
 
