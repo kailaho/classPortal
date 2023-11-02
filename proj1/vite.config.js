2518fa0095727268300fc1c16e34db0e434b68aa
kailaho <kaho3670@colorado.edu>
2023-11-01 19:20:43 -0600

    initial commit

---
+++ proj1/vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    outDir: 'dist',
+    assetsDir: '',
+    assetsInlineLimit: 0,
+  },
+  base: '/proj1/', 
+});
